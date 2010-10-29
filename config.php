695d5c16ae77ef08c79b298547b26bb51faad4d1
pasarian <pasarian@40d42ba9-90a9-8561-e80c-e3ba712781ec>
2010-10-29 09:00:22 +0000

    Выгрузка последней стабильной версии системы
    git-svn-id: http://tilla.googlecode.com/svn/trunk@8 40d42ba9-90a9-8561-e80c-e3ba712781ec

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+		$CONF["db_name"] = "dataBaseName";
+		$CONF["db_host"] = "dataBaseHost";
+		$CONF["db_login"] = "dataBaseLogin";
+		$CONF["db_pass"] = "dataBasePassword";
+		$CONF["db_prefix"] = "dataBasePrefix";
+		?>
