7b681a2d03ffb89332a7f6e41f53b79c07c0d1b6
pasarian <pasarian@40d42ba9-90a9-8561-e80c-e3ba712781ec>
2010-11-24 10:03:00 +0000

    Удалённый файл создётся системой при первом запуске админ-панели.
    git-svn-id: http://tilla.googlecode.com/svn/trunk@17 40d42ba9-90a9-8561-e80c-e3ba712781ec

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-		$CONF["db_name"] = "dataBaseName";
-		$CONF["db_host"] = "dataBaseHost";
-		$CONF["db_login"] = "dataBaseLogin";
-		$CONF["db_pass"] = "dataBasePassword";
-		$CONF["db_prefix"] = "dataBasePrefix";
-		?>
