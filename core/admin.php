458772310ed85b4258638ab4904bc37041268940
pasarian <pasarian@40d42ba9-90a9-8561-e80c-e3ba712781ec>
2010-12-22 11:45:29 +0000

    Подготовка к релизу версии 1.067
    git-svn-id: http://tilla.googlecode.com/svn/trunk@66 40d42ba9-90a9-8561-e80c-e3ba712781ec

--- core/admin.php
+++ core/admin.php
@@ -529,8 +529,8 @@ if (count($CONF)==0 or $_POST['action']=='createCONF'){
 /********************************************/
 /* Отображение данных о версии CMS			*/
 $_SESSION['cur_branch'] = 'branch1';
-$_SESSION['cur_version'] = '1.061 beta';
-$_SESSION['cur_number'] = 1061;
+$_SESSION['cur_version'] = '1.067 beta';
+$_SESSION['cur_number'] = 1067;
 /* Отображение данных о версии CMS			*/
 /********************************************/
 				
