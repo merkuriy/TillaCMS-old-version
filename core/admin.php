d1a6f025159dbe769e65123016e2c767ca4b1f97
333peps@gmail.com <333peps@gmail.com@40d42ba9-90a9-8561-e80c-e3ba712781ec>
2011-01-26 04:56:20 +0000

    Подготовка к релизу версии 1.096
    git-svn-id: http://tilla.googlecode.com/svn/trunk@95 40d42ba9-90a9-8561-e80c-e3ba712781ec

--- core/admin.php
+++ core/admin.php
@@ -529,8 +529,8 @@ if (count($CONF)==0 or $_POST['action']=='createCONF'){
 /********************************************/
 /* Отображение данных о версии CMS			*/
 $_SESSION['cur_branch'] = 'branch1';
-$_SESSION['cur_version'] = '1.093 beta';
-$_SESSION['cur_number'] = 1093;
+$_SESSION['cur_version'] = '1.096 beta';
+$_SESSION['cur_number'] = 1096;
 /* Отображение данных о версии CMS			*/
 /********************************************/
 				
